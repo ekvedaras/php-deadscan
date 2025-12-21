10c421d90e831b85d1a601aad13ba8dc9736fb94
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2025-12-21 13:35:35 +0200

    feat: add colors

--- src/scan/mod.rs
+++ src/scan/mod.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
+use owo_colors::OwoColorize;
 
 pub(crate) fn scan(path: &PathBuf, json: &bool) {
-    println!("Scanning: {:?}, --json?={:?}", path.display(), json);
+    println!("Scanning: {:?}, --json?={:?}", path.display().yellow(), json.yellow());
 }
