91af43fe180b7380d0ca0c6499371e365c74694a
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2025-12-22 14:42:16 +0200

    feat: iterate php files in the given path recursively

--- src/scan/mod.rs
+++ src/scan/mod.rs
@@ -1,6 +1,20 @@
-use std::path::PathBuf;
 use owo_colors::OwoColorize;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 pub(crate) fn scan(path: &PathBuf, json: &bool) {
-    println!("Scanning: {:?}, --json?={:?}", path.display().yellow(), json.yellow());
+    println!(
+        "Scanning: {:?}, --json?={:?}",
+        path.display().yellow(),
+        json.yellow()
+    );
+
+    for entry in WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .filter(|e| e.path().ends_with(".php"))
+    {
+        println!("{:?}", entry.path().display().yellow());
+    }
 }
