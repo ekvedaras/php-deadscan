8e47ad7a45f6f70bc0c373c7da723d05e9655efe
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2025-12-23 13:44:27 +0200

    feat: print table

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use cli::{Cli, Commands};
 use owo_colors::OwoColorize;
+use tabled::builder::Builder;
+use tabled::settings::Style;
 
 mod cli;
 mod scan;
@@ -26,7 +28,22 @@ fn main() {
         }) => {
             let unused_classes = scan::scan(path, json);
             match unused_classes {
-                Ok(classes) => println!("{:?}", classes),
+                Ok(classes) => {
+                    let mut builder = Builder::default();
+
+                    builder.insert_column(0, ["Unused class"]);
+                    builder.insert_column(1, ["Path"]);
+                    builder.insert_record(0, vec!["Unused class", "Path"]);
+
+                    classes.iter().for_each(|(k, path)| {
+                        builder.push_record(vec![k.clone(), path.display().to_string()])
+                    });
+
+                    let mut table = builder.build();
+                    table.with(Style::rounded());
+
+                    println!("{}", table);
+                }
                 Err(error) => println!("{}", error.red()),
             }
         }
--- src/scan.rs
+++ src/scan.rs
@@ -3,18 +3,18 @@ mod usage;
 
 use crate::scan::discover::discover_classes;
 use crate::scan::usage::detect_usages;
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
-pub(crate) fn scan(path: &PathBuf, _json: &bool) -> Result<HashSet<String>, String> {
+pub(crate) fn scan(path: &PathBuf, _json: &bool) -> Result<HashMap<String, PathBuf>, String> {
     let class_map = discover_classes(path)
         .map_err(|e| format!("Failed to discover classes: {}", e.to_string()))?;
     let usages =
         detect_usages(path).map_err(|e| format!("Failed to detect usages: {}", e.to_string()))?;
 
     Ok(class_map
-        .keys()
-        .filter(|k| !usages.contains_key(*k))
-        .cloned()
+        .iter()
+        .filter(|(k, _path)| !usages.contains_key(*k))
+        .map(|(k, v)| (k.clone(), v.clone()))
         .collect())
 }
